6bc75988c9626635185f0b1201f18be3dc771f8c
darkclif <morpheoosmatrix@gmail.com>
2018-03-02 09:42:53 +0100

    proyecto SUBIDO EN CLASE

---
+++ app/src/main/java/com/example/pruebasproyecto/AvanzadaActivity.java
@@ -0,0 +1,13 @@
+package com.example.pruebasproyecto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AvanzadaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avanzada);
+    }
+}
